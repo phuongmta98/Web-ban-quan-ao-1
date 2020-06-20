7d20e4f88ff63cd653b958e05f978aadfa75e77b
QuangHuyy <50898345+QuangHuyy@users.noreply.github.com>
2020-06-20 20:14:47 +0700

    Thêm danh sách hóa đơn
    + Hiển thị danh sách hóa đơn
    + Chi tiết của hóa đơn
    
    - Chưa thực hiện chốt đơn
    - Chưa xem được phản hồi
    - Giao diện còn xấu

---
+++ CNW_WebBanQuanAo/Models/CTHOADON.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CNW_WebBanQuanAo.Models
+{
+    public class CTHOADON
+    {
+        [Key]
+        [Column(Order = 0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int MaHD { get; set; }
+
+        [Key]
+        [Column(Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int MaQA { get; set; }
+
+        [StringLength(50)]
+        public string MaKH { get; set; }
+
+        [DisplayName("Ngày Đặt")]
+        public DateTime? NgayDat { get; set; }
+
+        public DateTime? NgayGiao { get; set; }
+
+        [StringLength(50)]
+        public string NguoiChot { get; set; }
+
+        [StringLength(50)]
+        [DisplayName("Trạng Thái")]
+        public string TrangThai { get; set; }
+
+        [StringLength(50)]
+        public string TenKhach { get; set; }
+
+        [StringLength(50)]
+        public string DiaChiKhach { get; set; }
+
+        public int SoLuong { get; set; }
+
+        public int? MaMH { get; set; }
+
+        [StringLength(10)]
+        public string MaSize { get; set; }
+
+        public int? MaMau { get; set; }
+
+        public virtual MATHANG MATHANG { get; set; }
+
+        public virtual MAU MAU { get; set; }
+
+        public virtual SIZE SIZE { get; set; }
+
+        public virtual TAIKHOAN TAIKHOAN { get; set; }
+    }
+}
